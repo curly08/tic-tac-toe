63caca4323e9550ff0a61744262800d0749e5cac
curly08 <mattynoakes08@gmail.com>
2021-10-21 16:10:02 -0400

    Add set_mark method

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+# player with name and mark attributes
 class Player
   def initialize(name)
     @name = name
     @mark = nil
+  end
+
+  def set_mark(value)
+    @mark = value
   end
 end
