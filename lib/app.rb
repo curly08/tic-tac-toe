05dc0c25194a963eb22a00f7d67272fce852f3d3
curly08 <mattynoakes08@gmail.com>
2021-10-23 21:24:32 -0400

    Refactor #play_move and add #input_valid?

--- lib/app.rb
+++ lib/app.rb
@@ -5,6 +5,7 @@ require_relative '../lib/board'
 
 # Game class
 class TicTacToe
+  attr_accessor :chosen_spots
   attr_reader :player_one, :player_two, :board
 
   def initialize(first_player_name, second_player_name)
@@ -12,45 +13,51 @@ class TicTacToe
     @player_one = Player.new(@arr[0], 'X')
     @player_two = Player.new(@arr[1], 'O')
     @board = Board.new
+    @chosen_spots = []
 
-    @possible_inputs = {
-      'top left': 1, 'top middle': 5, 'top right': 9,
-      'middle left': 13, 'middle middle': 17, 'middle right': 21,
-      'bottom left': 25, 'bottom middle': 29, 'bottom right': 33
-    }
+    # @possible_inputs = {
+    #   'top left': 1, 'top middle': 5, 'top right': 9,
+    #   'middle left': 13, 'middle middle': 17, 'middle right': 21,
+    #   'bottom left': 25, 'bottom middle': 29, 'bottom right': 33
+    # }
   end
 
   def play_game
     puts board.display
-    until game_over? do
-      player_one.turn_count == player_two.turn_count ? player_one.play_move : player_two.play_move
+    until game_over?
+      player_one.turn_count == player_two.turn_count ? play_move(player_one) : play_move(player_two)
       puts board.update_display
     end
     ending_message
   end
 
-  # move method
-  def play_move(name, mark)
-    puts "#{name}, where would you like to place your mark?"
-    @input = gets.chomp.strip
-    place_mark(@input, mark)
-    puts show_board
-    check_for_win(name, mark)
+  def play_move(player, input = nil)
+    until input_valid?(input)
+      puts "#{player.name}, where would you like to place your mark?"
+      input = gets.chomp
+    end
+    place_mark(player, input)
+    chosen_spots << input
+    player.increase_turn_count
   end
 
-  def place_mark(input, mark)
-    if @possible_inputs.include?(input.to_sym)
-      @possible_inputs.each_pair do |possible_input, index|
-        if input == possible_input.to_s
-          @board.delete_at(index)
-          @board.insert(index, mark)
-          @possible_inputs.delete(possible_input)
-          break
-        end
-      end
-    else
-      invalid_input(mark)
-    end
+  def input_valid?(input)
+    input.to_i.between?(1, 9) && !chosen_spots.include?(input) ? true : false
+  end
+
+  def place_mark(player, input)
+    # if @possible_inputs.include?(input.to_sym)
+    #   @possible_inputs.each_pair do |possible_input, index|
+    #     if input == possible_input.to_s
+    #       @board.delete_at(index)
+    #       @board.insert(index, mark)
+    #       @possible_inputs.delete(possible_input)
+    #       break
+    #     end
+    #   end
+    # else
+    #   invalid_input(mark)
+    # end
   end
 
   def invalid_input(mark)
