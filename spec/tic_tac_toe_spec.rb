3a31f6e81b59f8f3408b505a4956c21e7318a6e7
curly08 <mattynoakes08@gmail.com>
2021-10-25 09:31:32 -0400

    Add tests for #check_for_win and #check_for_tie, and clean up file

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -40,21 +40,32 @@ describe TicTacToe do
   end
 
   describe '#play_game' do
-    context 'game_over is true' do
-
+    context '#game_over is true' do
       before do
-        allow(game).to receive(:puts).and_return(game.board.display)
+        allow(game).to receive(:puts)
         allow(game).to receive(:game_over?).and_return(true)
       end
 
-      it 'calls #ending_message' do
-        expect(game).to receive(:ending_message)
+      it 'does not call methods within' do
+        expect(game).not_to receive(:play_move)
+        expect(game).to receive(:puts).once
+        expect(game).not_to receive(:check_for_win)
+        expect(game).not_to receive(:check_for_tie)
         game.play_game
       end
+    end
 
-      it 'does not call play_move or update_display' do
-        expect(game).not_to receive(:play_move)
-        expect(game.board).not_to receive(:update_display)
+    context '#game_over is false' do
+      before do
+        allow(game).to receive(:puts)
+        allow(game).to receive(:game_over?).and_return(false, true)
+      end
+
+      it 'does call methods within' do
+        expect(game).to receive(:play_move)
+        expect(game).to receive(:puts)
+        expect(game).to receive(:check_for_win)
+        expect(game).to receive(:check_for_tie)
         game.play_game
       end
     end
@@ -65,17 +76,18 @@ describe TicTacToe do
       input = '3'
 
       before do
-        allow(game).to receive(:input_valid?).and_return(false, true)
+        allow(game).to receive(:input_valid?).and_return(true)
         allow(game).to receive(:puts)
         allow(game).to receive(:gets).and_return(input)
+        allow(game).to receive(:place_mark)
       end
 
       it 'loop ends after 1 try' do
         expect(game).to receive(:puts).once
         expect(game).to receive(:gets).and_return(input).once
+        expect(game).to receive(:place_mark).once
         game.play_move(game.player_one)
       end
-
 
       it 'adds input to chosen_spots' do
         expect {game.play_move(game.player_one)}.to change { game.chosen_spots }.from([]).to([input])
@@ -83,6 +95,11 @@ describe TicTacToe do
 
       it 'increases player turn count' do
         expect(game.player_one).to receive(:increase_turn_count)
+        game.play_move(game.player_one)
+      end
+
+      it 'updates player mark history' do
+        expect(game.player_one).to receive(:update_mark_history).with(input)
         game.play_move(game.player_one)
       end
     end
@@ -94,15 +111,11 @@ describe TicTacToe do
         allow(game).to receive(:input_valid?).and_return(false, false, true)
         allow(game).to receive(:puts)
         allow(game).to receive(:gets).and_return(input)
+        allow(game).to receive(:place_mark)
       end
 
       it 'runs loop twice' do
         expect(game).to receive(:input_valid?).exactly(3).times
-        game.play_move(game.player_one)
-      end
-
-      it 'does not increase player turn_count' do
-        expect(game.player_one).to receive(:increase_turn_count)
         game.play_move(game.player_one)
       end
     end
@@ -114,6 +127,7 @@ describe TicTacToe do
         allow(game).to receive(:input_valid?).and_return(false, false, false, true)
         allow(game).to receive(:puts)
         allow(game).to receive(:gets).and_return(input)
+        allow(game).to receive(:place_mark)
       end
 
       it 'runs loop three times' do
@@ -133,19 +147,96 @@ describe TicTacToe do
 
     context 'input is a chosen board location' do
       before do
+        allow(game).to receive(:puts)
         game.chosen_spots = ['3']
       end
 
-      it 'returns false' do
+      it 'does not return true' do
         result = game.input_valid?('3')
-        expect(result).to be(false)
+        expect(result).not_to be(true)
       end
     end
 
     context 'input is not a number' do
-      it 'returns false' do
+      before do
+        allow(game).to receive(:puts)
+      end
+
+      it 'does not return true' do
         result = game.input_valid?('d')
-        expect(result).to be(false)
+        expect(result).not_to be(true)
+      end
+    end
+  end
+
+  describe '#check_for_win' do
+    context 'when row is won' do
+      before do
+        allow(game).to receive(:puts)
+        game.player_one.marked_locations = %w[1 2 3]
+      end
+
+      it 'changes game.game_over to true' do
+        expect { game.check_for_win(game.player_one) }.to change { game.game_over }.from(false).to(true)
+      end
+    end
+
+    context 'when diagonal is won' do
+      before do
+        allow(game).to receive(:puts)
+        game.player_one.marked_locations = %w[9 1 5]
+      end
+
+      it 'changes game.game_over to true' do
+        expect { game.check_for_win(game.player_one) }.to change { game.game_over }.from(false).to(true)
+      end
+    end
+
+    context 'when column is won' do
+      before do
+        allow(game).to receive(:puts)
+        game.player_one.marked_locations = %w[7 4 2 6 1]
+      end
+
+      it 'changes game.game_over to true' do
+        expect { game.check_for_win(game.player_one) }.to change { game.game_over }.from(false).to(true)
+      end
+    end
+
+    context 'no winning condition is met' do
+      before do
+        game.player_one.marked_locations = %w[6 1 7 9]
+      end
+
+      it 'does not change game.game_over' do
+        expect { game.check_for_win(game.player_one) }.not_to change { game.game_over }
+      end
+    end
+  end
+
+  describe '#check_for_tie' do
+    context 'when there is a cats game' do
+      before do
+        game.player_one.marked_locations = %w[6 1 7 9]
+        game.player_two.marked_locations = %w[6 1 7 9]
+        game.chosen_spots = %w[6 1 7 9 3 2 5 4 8]
+        allow(game).to receive(:puts)
+      end
+
+      it 'changes game.game_over to true' do
+        expect {game.check_for_tie}.to change {game.game_over}.from(false).to(true)
+      end
+    end
+
+    context 'when there is no cats game' do
+      before do
+        game.player_one.marked_locations = %w[6 1 7 9]
+        game.player_two.marked_locations = %w[6 1 7 9]
+        game.chosen_spots = %w[6 1 7 9 3 2 4 8]
+      end
+
+      it 'game_over does not change' do
+        expect {game.check_for_tie}.not_to change {game.game_over}
       end
     end
   end
